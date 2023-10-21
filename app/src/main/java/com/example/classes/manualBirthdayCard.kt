d04d0861aafbf106542e023778b84c05a20f3384
igyccmt <ugurlualeyna@gmail.com>
2023-10-21 14:59:56 +0300

    Elle yazıldığında bu şekilde bir kod yazabiliriz. Peki, daha kolay yolları nelerdir?

---
+++ app/src/main/java/com/example/classes/manualBirthdayCard.kt
@@ -0,0 +1,16 @@
+package com.example.classes
+
+fun main() {
+    val isim = "Hakan"
+    val yas = "23"
+    println("İyi ki doğdun $isim!")
+    println("Artık $yas yaşındasın!!!")
+    println("-------------------------")
+    println(" ''''''''''''''''''''''' ")
+    println(" ||||||||||||||||||||||| ")
+    println("=========================")
+    println("@@@@@@@@@@@@@@@@@@@@@@@@@")
+    println("@@@@@@@@@@@@@@@@@@@@@@@@@")
+    println("@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+}
